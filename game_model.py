3fa90d122a68322d09070f75601080a2147dd03a
ethanMan <ethan.y.man@gmail.com>
2025-04-01 16:30:47 -0400

    more framework

--- game_model.py
+++ game_model.py
@@ -33,7 +33,7 @@ class Grid:
 
     def get_tile(self, row, col):
         return self.grid[row][col]
-        
+
     def set_tile(self, row, col, tile_state):
         self.grid[row][col].set_state(tile_state)
     
@@ -42,12 +42,42 @@ class GameModel:
         self.grid=Grid()
         self.score=0
         self.current_shapes=[]
-    # place block (placing many tiles)
-        # Check if each tile is free
-    # Calculate score
-    # Go to next round (get three new shapes)
-    # Check if the game is over
-    # 
+
+    def get_grid(self):
+        return self.grid
+
+    def get_score(self):
+        return self.score
+
+    def get_current_shapes(self):
+        return self.current_shapes
+
+    def new_round(self):
+        # Get three new shapes
+        # Check if the game is over
+        pass
+
+    def place_block(self, block):
+        # Check if the block can be placed
+        # Place the block on the grid
+        # Update the score
+        pass
+
+    def check_game_over(self):
+        # Check if there are any empty tiles left
+        # Check if there are any blocks that can be placed
+        pass
+
+    def check_block_placement(self, block):
+        # Check if the block can be placed on the grid
+        # Check if the block is within the grid boundaries
+        # Check if the block overlaps with any occupied tiles
+        pass
+
+    def calculate_score(self):
+        # Calculate the score based on the number of tiles occupied
+        # and the number of blocks placed
+        pass
 
     
             

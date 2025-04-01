5a35115c5f0f1712accb64e9c4d56aa95638af02
ethanMan <ethan.y.man@gmail.com>
2025-04-01 16:48:33 -0400

    placing block logic kinda wow idk what Im doing

--- game_model.py
+++ game_model.py
@@ -14,7 +14,6 @@ class GridTile:
 
 
 class Grid:
-    
     def __init__(self) -> None:
         self.grid=self.initialize_grid()
     
@@ -36,8 +35,14 @@ class Grid:
 
     def set_tile(self, row, col, tile_state):
         self.grid[row][col].set_state(tile_state)
-    
+
+
 class GameModel:
+    """
+        The GameModel class represents the game model for a grid-based game.
+        It manages the game state, including the grid, score, and current shapes.
+    """
+
     def __init__(self) -> None:
         self.grid=Grid()
         self.score=0
@@ -53,14 +58,35 @@ class GameModel:
         return self.current_shapes
 
     def new_round(self):
-        # Get three new shapes
+        # Get three new shapes (where it is possible to win the game?)
+        for _ in range(3):
+            shape=self.get_random_shape()
+            self.current_shapes.append(shape)
         # Check if the game is over
+        self.check_game_over()
         pass
 
-    def place_block(self, block):
+    def place_block(self, row, col, block):
         # Check if the block can be placed
+        block_indices = block.get_indices()
+
         # Place the block on the grid
         # Update the score
+        pass
+
+    def can_place_block(self, row, col, block):
+        # Check if the block can be placed on the grid
+        block_indices = block.get_indices()
+        # TODO: implement block logic
+        for i in block_indices:
+            if row + i[0] < 0 or row + i[0] >= 8 or col + i[1] < 0 or col + i[1] >= 8:
+                return False
+            tile = self.grid.get_tile(row + i[0], col + i[1])
+            if tile.get_occupied():
+                return False
+        return True
+        # Check if the block is within the grid boundaries
+        # Check if the block overlaps with any occupied tiles
         pass
 
     def check_game_over(self):
@@ -77,6 +103,11 @@ class GameModel:
     def calculate_score(self):
         # Calculate the score based on the number of tiles occupied
         # and the number of blocks placed
+        pass
+
+    def get_random_shape(self):
+        # Generate a random shape
+        # Return the shape
         pass
 
     

6977d067341e0acd61fe1b9163832e38814958d9
Ethan Man <ethan.y.man@gmail.com>
2025-04-01 16:16:29 -0400

    new class + new set/get

--- game_model.py
+++ game_model.py
@@ -16,14 +16,10 @@ class GridTile:
 class Grid:
     
     def __init__(self) -> None:
-        
         self.grid=self.initialize_grid()
     
     
-    
-    
     def initialize_grid(self):
-        
         grid=[]
         
         for i in range(8):
@@ -34,7 +30,25 @@ class Grid:
             grid.append(row)
         
         return grid
-            
-            
+
+    def get_tile(self, row, col):
+        return self.grid[row][col]
+        
+    def set_tile(self, row, col, tile_state):
+        self.grid[row][col].set_state(tile_state)
+    
+class GameModel:
+    def __init__(self) -> None:
+        self.grid=Grid()
+        self.score=0
+        self.current_shapes=[]
+    # place block (placing many tiles)
+        # Check if each tile is free
+    # Calculate score
+    # Go to next round (get three new shapes)
+    # Check if the game is over
+    # 
+
+    
             
         
